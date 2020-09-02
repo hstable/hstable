52ea4b99cd6fa1a19dffe534c4f90756cc094f02
crazcell <crazcell@yandex.com>
2020-09-03 03:03:16 +0800

    fix: fix a problem that not redirect to login

--- ui/middleware/auth.js
+++ ui/middleware/auth.js
@@ -1,6 +1,6 @@
 export default function ({ store, redirect }) {
   // If the user is not authenticated
-  if (!localStorage.token && !location.path.startsWith('/login')) {
+  if (!localStorage.token && !location.pathname.startsWith('/login')) {
     return redirect('/login')
   }
 }
